16cd05921259d5cf8c26734503946cc8508f9e0a
jem <jhonemillan@gmail.com>
2018-03-09 01:59:19 -0500

    implement map

--- src/app/apiconfig.ts
+++ src/app/apiconfig.ts
@@ -1,1 +1,1 @@
-export var key = 'AIzaSyCCAfMLuhU5or6mKeIhIvE-ELMSWqrLUjI' 
+export var key = 'AIzaSyACXYjL1DAOyjT1Hf0ulkNAvtpU1R4oFsQ' 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule, MatSidenav} from '@angular/material/sidenav';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,25 @@ import {MatSidenavModule, MatSidenav} from '@angular/material/sidenav';
 })
 export class AppComponent {
   title = 'app';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number =0;
+  lng: number = 0;  
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+
+  ngOnInit() {
+    this.findMe();
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude; 
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  
 }
