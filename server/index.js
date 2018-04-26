53836be24983f3546125486bf54535f2416c675e
jem <jhonemillan@gmail.com>
2018-04-26 01:20:34 -0500

    session

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const exsession = require("express-session");
 const passport = require('passport');
 const app = express();
 const port = process.env.port || 3000;
+const MongoDBStore = require('connect-mongodb-session')(exsession);
 let twitterPassport = require('./security/twitter');
 let db = require('./security/config')
 app.use(logger('dev'));
@@ -19,15 +20,22 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+var store = new MongoDBStore(
+    {
+      uri: db.database,
+      databaseName: 'geocrimes',
+      collection: 'Sessions'
+    });
 
-app.use(exsession({ secret: "victoria", 
+app.use(exsession({ secret: db.secretApp, 
                     saveUninitialized: true, 
+                    store: store,
                     resave: true }));
                     
 app.use(passport.initialize());
 app.use(passport.session());  
 twitterPassport(passport);
-mongoose.connect('mongodb://localhost/geocrimes');
+mongoose.connect(db.database);
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
 
@@ -36,6 +44,30 @@ app.get('/auth/twitter/callback',
                                       failureRedirect: '/mal',
                                       failureFlash: true }));
 
+app.get('/logout', function (req, res){
+  req.session.destroy(function (err) {
+      if (err) {
+          console.log(err);
+      }
+    res.redirect('/'); //Inside a callback… bulletproof!
+  });
+  req.session = null
+});
+
+function ensureAuthenticated(req, res, next) {
+    console.log(req.user);
+    if (req.authenticated()) {
+      
+      return next(); 
+    }
+  
+    // denied. redirect to login
+    res.redirect('/')
+  }
+
+app.get('/test',ensureAuthenticated, (req, res)=>{
+console.log('si puedo entrar')
+})
 
 app.listen(port, ()=>{
     console.log('Se conecta al puerto ' + port)
@@ -54,5 +86,11 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
 });
+
+// Catch errors
+store.on('error', function(error) {
+    assert.ifError(error);
+    assert.ok(false);
+  });
 
 module.exports = app;    
--- server/security/config.js
+++ server/security/config.js
@@ -13,5 +13,7 @@ module.exports = {
         consumerKey: '769cexw3AY6dsx7jG9klm6peN',
         consumerSecret: 'C93fSh7T6ZCX49LxywP1shOFkmHPTshk5wmt3JMJDhthlVZwV1',
         callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    }
+    },
+    secretApp : 'victoria',
+    database : 'mongodb://localhost/geocrimes'
 };
